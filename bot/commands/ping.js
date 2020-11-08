9b131b7c63a144e1b9de17d84337533344612ef7
cfp <chaosflo44plus@gmail.com>
2020-11-08 17:43:33 +0100

    Update ping.js

--- bot/commands/ping.js
+++ bot/commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
-
+        message.reply("PONG!!! IT WORKS")
     },
     permissions: [],
     requiredRoles: []
