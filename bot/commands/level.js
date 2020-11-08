5c27ea59bee10e841867ba33da166308048eb987
cfp <chaosflo44plus@gmail.com>
2020-11-08 18:46:15 +0100

    Update level.js

--- bot/commands/level.js
+++ bot/commands/level.js
@@ -19,7 +19,7 @@ module.exports = {
     permissionError: 'I dont think you should be able to execute this command :grin:',
     minArgs: 0,
     maxArgs: 0,
-    callback: (message, arguments, text) => {
+    callback: await function(message, arguments, text) {
         let user = message.mentions.users.first() || message.author;
 
         if(!xpfile[user.id]) {
